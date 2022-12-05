afe0d57672d6eea42f7ecca604fcc498ae981499
C0dingChris <Chris.Sanger@outlook.com>
2022-12-05 15:14:00 +0000

    changed app logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://www.logodesign.net/images/home-page-logo-03.png"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
